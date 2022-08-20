8bf62534d7aeb5b27412d66abe5e541f7d5777a7
“Vidit <vidit.21102@gmail.com>
2022-08-20 16:49:46 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: LoginComponent,
+  path: "login"
+},
+{
+  component : SignupComponent,
+  path : "signup"
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
